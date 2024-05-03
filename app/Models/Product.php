574e062d5345dca13cf9c4db642311070a522f80
Matheus Júnior <matheusfpjunior@gmail.com>
2024-05-02 21:36:26 -0300

    feat: initial commit, create byte-service structure

---
+++ app/Models/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        "id_product",
+        "category_id",
+        "name",
+        "price",
+        "sale_price",
+        "image",
+        "description",
+        "active",
+        "tag",
+        "rank"
+    ];
+}
