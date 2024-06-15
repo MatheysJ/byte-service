66cb14bfd843badf13a8cb0f29aab27ddf694e6c
Matheus Júnior <matheusfpjunior@gmail.com>
2024-06-15 19:18:40 -0300

    Create auth logic and protect post, put and delete methods

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -34,16 +34,6 @@ class ProductController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -63,17 +53,6 @@ class ProductController extends Controller
     public function show($id)
     {   
         return Product::with("category")->findOrFail($id);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
     }
 
     /**
